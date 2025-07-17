9eff838c94be2c26d014602a8ae73ec39eb4078d
Davi <davidias0881@gmail.com>
2025-07-16 21:48:42 -0300

    PROJETO COMPLETO

--- EmpregaSenac/settings.py
+++ EmpregaSenac/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iq0b(5t&xai$1pl0q-vk5ko_*a%76!hf42)n@w2qhe+ywagd0a'
+import os
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +78,20 @@ WSGI_APPLICATION = 'EmpregaSenac.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# empregasenac/settings.py
+# ...
+# empregasenac/settings.py
+# ... # <--- ADICIONE ESTA LINHA
+# ... # <--- ADICIONE ESTA LINHA
+# ...
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
