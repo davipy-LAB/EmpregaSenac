1b153e2d8c0fa5d39220c9036853c1bb7258b205
Davi <davidias0881@gmail.com>
2025-07-18 12:39:49 -0300

    MUDANÇA DE DATABASE

--- EmpregaSenac/settings.py
+++ EmpregaSenac/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'EmpregaSenac.wsgi.application'
 # ... # <--- ADICIONE ESTA LINHA
 # ... # <--- ADICIONE ESTA LINHA
 # ...
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 
